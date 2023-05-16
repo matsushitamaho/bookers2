917a1672fc204ee9cbee7ae06c601371952f01fe
mathushitamaho <132318410+matsushitamaho@users.noreply.github.com>
2023-05-16 08:02:56 +0000

    booksコントローラ作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_16_012704) do
+ActiveRecord::Schema.define(version: 2023_05_16_071344) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
